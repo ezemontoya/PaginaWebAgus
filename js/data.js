9665e72b479e66cc28be6f251485ddb459295120
ezemontoya <essecba@gmail.com>
2021-04-13 00:50:25 -0300

    Agus Working

---
+++ js/data.js
@@ -0,0 +1,35 @@
+//  SECCIÓN DE DECLARACIÓN DE INFORMACIÓN ESTÁTICA
+let listaProductos = [{
+  "id": 1,
+  "nombre": "Mini Conservadora",
+  "avatar": "imagenes/mini Conservadora.JPG",
+  "precio": 2000
+ }, {
+  "id": 2,
+  "nombre": "Caja de pesca",
+  "avatar": "imagenes/caja de pesca.jpeg",
+  "precio": 2500
+}, {
+  "id": 3,
+  "nombre": "Destapadores de pared",
+  "avatar": "imagenes/Destapadores.JPG",
+  "precio": 850
+}, {
+  "id": 4,
+  "nombre": "Conservadora Grande",
+  "avatar": "imagenes/Conservadora grande.jpeg",
+  "precio": 3500
+}, {
+  "id": 5,
+  "nombre": "Pie porta maceta",
+  "avatar": "imagenes/Pie porta maceta.jpeg",
+  "precio": 1500
+}, {
+  "id": 6,
+  "nombre": "Porta maceta triple",
+  "avatar": "imagenes/Porta maceta triple.jpeg",
+  "precio": 3200
+}];
+
+
+let SELECCIONADOS = [];
